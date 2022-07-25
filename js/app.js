bb153d3f636fcc3e643b610f500bb993ede4c7a5
beepek <ben.schylling@gmail.com>
2022-07-25 14:32:13 -0400

    push4

--- js/app.js
+++ js/app.js
@@ -10,6 +10,19 @@ const eatLvl = document.querySelector("#food-stats");
 const advLvl = document.querySelector("#adventure-stats");
 const statusBar = document.querySelector("#stats")
 /*----- app's state (variables) -----*/
+//state variables/init variables
+const startSleep = 2;
+const startFood = 2;// these are the values at start of game, or whenever browser is run
+const startAdv = 2;
+//day ticker
+let daysLived = 1;
+function init()
+function hero() {
+    this.sleep = startSleep;
+    this.eat = startFood;
+    this.adventure = startAdv;
+}
+console.log(hero);
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 /*----- functions -----*/
