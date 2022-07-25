dc1248ba5e390b5de801e6ae3fdb4aa67e37d81a
beepek <ben.schylling@gmail.com>
2022-07-25 13:58:42 -0400

    push3

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 /*----- constants -----*/
+//all the buttons
+const sleepBtn = document.querySelector("#action-sleep");
+const eatBtn = document.querySelector("#action-eat");
+const advBtn = document.querySelector("#action-adventure");
+//consts for main stats
+
+const sleepLvl = document.querySelector("#sleep-stats");
+const eatLvl = document.querySelector("#food-stats");
+const advLvl = document.querySelector("#adventure-stats");
+const statusBar = document.querySelector("#stats")
 /*----- app's state (variables) -----*/
 /*----- cached element references -----*/
 /*----- event listeners -----*/
